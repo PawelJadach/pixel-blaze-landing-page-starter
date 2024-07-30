5dc89d115977f2afcedfb1478036292ef2d7ac80
Pawel Jadach <pawel.jadach@empik.com>
2024-07-30 15:40:32 +0200

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,49 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { config } from "@/config/config";
+import { Analytics } from "@vercel/analytics/react";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import { Inter } from "next/font/google";
+import { cn } from "@/utils/cn";
+
+const font = Inter({ subsets: ["latin"] });
+export const metadata: Metadata = config.metadata.root;
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang={config.locale} className="scroll-smooth scroll-pt-32">
+			<head>
+				<link
+					rel="apple-touch-icon"
+					sizes="180x180"
+					href="/apple-touch-icon.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="32x32"
+					href="/favicon-32x32.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="16x16"
+					href="/favicon-16x16.png"
+				/>
+				<link rel="manifest" href="/site.webmanifest" />
+				<meta name="google" content="notranslate" />
+			</head>
+			<body className={cn(font.className, "min-h-screen mt-32")}>
+				<Navbar />
+				<div>{children}</div>
+				<Footer />
+				<Analytics />
+			</body>
+		</html>
+	);
+}
